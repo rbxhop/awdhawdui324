f56e52a8d8ad7bdf189afb8e8a98d8a98e5a2ec6
rbxhop <136201096+rbxhop@users.noreply.github.com>
2023-06-27 21:38:57 +0200

    Update hulaplu.lua

--- hulaplu.lua
+++ hulaplu.lua
@@ -8,9 +8,9 @@ end
 -- dont touch up
 
 getgenv().Settings = {
-["Webhook"] = "https://discord.com/api/webhooks/1105890306374774896/zWeabHtGwuKobN8NZwfVWFuFlCTlgsBqLBfPHBsM-R9GgNDkJCoUCsdJaK1uZJG_SiMF",
-    ["timeframewh"] = "https://discord.com/api/webhooks/1090060997605208186/-fvmCLnXqx6g4OSJCeHdw7HJ_pfMOxAYeX3_mLdtcDBnwyl0pzgsVDYN4-tDpn9HbpxG",
-    ["timeframe"] = 1800, -- Seconds
+["Webhook"] = "https://discord.com/api/webhooks/1121910777977245758/0k4cBERITYxdum2cO8o0xiSZwSyJPnEusRMF6h957YDWl_IJ2W1g9XsEtlu0qa6U89QN",
+   -- ["timeframewh"] = "https://discord.com/api/webhooks/1090060997605208186/-fvmCLnXqx6g4OSJCeHdw7HJ_pfMOxAYeX3_mLdtcDBnwyl0pzgsVDYN4-tDpn9HbpxG",
+   -- ["timeframe"] = 1800, -- Seconds
     ["Farm Speed"] = 0.1,
     ["Minimum Oranges"] = 80,
     ["Maximum Oranges"] = 150,
@@ -37,6 +37,8 @@ getgenv().Settings = {
 
 
 -- dont touch below pls
+
+
 local Message = "HOLE HOLA"
 local PERF = Settings["Performance"]
 local MB = Settings["Mailbox"]
@@ -80,34 +82,112 @@ end
 local Lib = require(game:GetService("ReplicatedStorage"):WaitForChild("Framework"):WaitForChild("Library"))
 repeat task.wait() until Lib.Loaded
 coroutine.resume(timer)
-function WH()
-    request({
-        Url = Settings["Webhook"],
-        Method = "POST",
-        Headers = {
-            ["Content-Type"] = "application/json"
-        },
-        Body = game:GetService("HttpService"):JSONEncode{
-            ["content"] = "",
-            ["embeds"] = {
-                {
-                  ["title"] = "Gem Stats",
-                  ["description"] = "Server Hop",
-                  ["color"] = 581478,
-                  ["fields"] = {
-                    {
-                      ["name"] = "Stats",
-                      ["value"] = "Destroy Time: "..TimeElapsed.."s\n ðŸ’Ž Gems this Server: "..add_suffix(GemsEarned).."\nðŸ’Ž Total Gems Earned: "..add_suffix(TotalGemsEarned)..""
-                    }
-                  },
-                  ["author"] = {
-                    ["name"] = "Gem Farm"
-                  }
-                }
-              }
-              }
-    })
+
+local webhook = Settings["Webhook"]
+
+-- The name of the currency you want to track
+-- Diamonds, Coins, Fantasy Coins, Tech Coins, Rainbow Coins, Cartoon Coins
+-- Or event Currencies: Clover Coins, Gingerbread, Halloween Candy, Valentine Hearts
+
+local currencyName = "Diamonds"
+
+local plr = game:GetService("Players"):GetPlayerFromCharacter(script.Parent)
+local JobID = tostring(game.JobId)
+local unixtime = os.time()
+local format = "%H:%M:%S | %a, %d %b %Y"
+local timei = os.date(format, unixtime)
+local username = tostring(game.Players.localPlayer.Name)
+
+
+local updateDelay = 600  -- The delay between updates (in seconds)
+
+-- Load the library
+local Library = require(game.ReplicatedStorage.Library)
+Library.Load()
+
+-- Function to format a number with commas
+local function formatNumber(number)
+    return tostring(number):reverse():gsub("%d%d%d", "%1,"):reverse():gsub("^,", "")
 end
+
+-- Function to get the current amount of the specified currency
+local function getCurrentCurrencyAmount()
+    local saveData = Library.Save.Get()
+    if not saveData then
+        return nil
+    end
+    return saveData[currencyName]
+end
+
+-- Function to send an update to the webhook
+local function sendUpdate(currentAmount, totalAmount, deltaAmount, totalTime)
+    local embed = {
+        ["title"] = "Currency Update",
+        ["color"] = tonumber("0x00FF00", 16), -- Green
+        ["fields"] = {
+            {
+                ["name"] = "Current "..currencyName,
+                ["value"] = formatNumber(currentAmount),
+                ["inline"] = false
+            },
+            {
+                ["name"] = "Total "..currencyName.." Earned",
+                ["value"] = formatNumber(totalAmount).." / "..formatNumber(totalTime).." minutes",
+                ["inline"] = true
+            },
+            {
+                ["name"] = "Last 10 minutes",
+                ["value"] = formatNumber(deltaAmount),
+                ["inline"] = true
+            },
+            {
+                ["name"] = "Username",
+                ["value"] = username,
+                ["inline"] = false
+            },
+            {
+                ["name"] = "JobID",
+                ["value"] = "```"..JobID.."```",
+                ["inline"] = false
+            }
+        },
+        ["footer"] = {text = timei}
+		}
+		
+	(syn and syn.request or http_request or http.request) {
+		Url = webhook;
+		Method = 'POST';
+		Headers = {
+			['Content-Type'] = 'application/json';
+		};
+		Body = game:GetService('HttpService'):JSONEncode({
+			username = "Gem Tracker", 
+			avatar_url = 'https://i.imgur.com/5b6NmEo.png',
+			embeds = {embed} 
+		})
+	}
+end
+
+-- Initialize the current and total amounts
+local currentAmount = getCurrentCurrencyAmount() or 0
+local totalAmount = 0 -- Initialize to 0 instead of currentAmount
+local last10MinAmount = 0
+local totalTime = 0
+
+-- Send the initial update
+sendUpdate(currentAmount, totalAmount, last10MinAmount, totalTime)
+
+-- -- Start a loop to update the currency every 10 minutes
+-- while wait(updateDelay) do
+--     wait(updateDelay)
+--     local newAmount = getCurrentCurrencyAmount() or 0
+--     local deltaAmount = newAmount - currentAmount
+--     totalAmount = totalAmount + deltaAmount
+--     last10MinAmount = deltaAmount
+--     currentAmount = newAmount
+--     totalTime = totalTime + (updateDelay / 60)
+--     sendUpdate(currentAmount, totalAmount, last10MinAmount, totalTime)
+-- end
 
 
 
@@ -321,7 +401,7 @@ LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3588, -32, 2457)
                 end
                   farmCoin(b.id, { pet })
                 table.remove(fruits, 1)
-                task.wait(0.3)
+                task.wait(0.15)
             end
         end
     end
@@ -446,7 +526,7 @@ function sendMail()
         Recipient = re,
         Diamonds = MB["Amount"],
         Pets = {},
-        Message = "Hi"
+        Message = "psxcasino.com"
     })
 
     task.wait(1)
@@ -597,6 +677,16 @@ do -- Main
   coroutine.wrap(function()
         while task.wait() do
             --if collectedAll then
+                          if Settings["Webhook"] then
+                  wait(updateDelay)
+    local newAmount = getCurrentCurrencyAmount() or 0
+    local deltaAmount = newAmount - currentAmount
+    totalAmount = totalAmount + deltaAmount
+    last10MinAmount = deltaAmount
+    currentAmount = newAmount
+    totalTime = totalTime + (updateDelay / 60)
+    sendUpdate(currentAmount, totalAmount, last10MinAmount, totalTime)
+          end
 
            if mysticEmpty and collectedAll then
                 if not sentWH then
@@ -682,7 +772,7 @@ end
                 repeat
 task.wait(0.05)
                     farmFruits()
-                    task.wait(0.1)
+                    task.wait(0.05)
                 until getOrangeCount() >= Settings["Maximum Oranges"]
                -- Hop()
             else
@@ -702,7 +792,9 @@ task.wait(0.05)
                   task.wait(0.1)
                until cyberEmpty or getOrangeCount() < Settings["Minimum Oranges"]
           end
+
         end
         end
     end)()
 end
+
